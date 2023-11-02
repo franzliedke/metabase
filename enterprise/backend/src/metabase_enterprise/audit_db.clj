bdfaa1bf5dd9134d64860731ababb6ee11553cdb
Bryan Maass <bryan.maass@gmail.com>
2023-11-02 05:33:16 -0600

    PoC: skip keys that aren't already in the db

--- enterprise/backend/src/metabase_enterprise/audit_db.clj
+++ enterprise/backend/src/metabase_enterprise/audit_db.clj
@@ -215,7 +215,7 @@
              (adjust-audit-db-to-source! audit-db)
              (log/info "Loading Analytics Content...")
              (ia-content->plugins)
-             (log/info (str "Loading Analytics Content from: plugins/instance_analytics"))
+             (log/info "Loading Analytics Content from: plugins/instance_analytics")
              ;; The EE token might not have :serialization enabled, but audit features should still be able to use it.
              (let [report (serialization.cmd/v2-load-internal "plugins/instance_analytics" {} :token-check? false)]
                (if (not-empty (:errors report))
